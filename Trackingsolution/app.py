620f9a53183d21044cf675b6c3668845ea60a0da
hvitklett <jr52vw@student.aau.dk>
2023-11-24 10:14:43 +0100

    Hello

--- Trackingsolution/app.py
+++ Trackingsolution/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template #Makes it posible to use templates
 app = Flask (__name__) #serving app from this file
 
 @app.route("/") #routes are what you type at the end op url, fx. search, maps. / is default when you visit the web page, and it calles the index function (landingpage)
-def index(): 
+def landingpage(): #Function called index, when you run it it renders a template called landingpage (when the user visits a specific route, the function returns to them some html content)
     return render_template("landingpage.html")
 
 @app.route("/seizures")
@@ -24,4 +24,4 @@ def kalender():
 
 #You run the web application by first going into the right directory, and then type flask run (not the folder name, but just flask everytime)
 
-#push
+#The render template function can also pass in variables to our html files
